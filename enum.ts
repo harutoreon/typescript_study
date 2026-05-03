4fb204cf29950aaf07104a6333ef8aa9435f18f2
reonharuto <norabder567@gmail.com>
2026-05-04 08:50:16 +0900

    Add enum.ts

---
+++ enum.ts
@@ -0,0 +1,9 @@
+enum Color {
+  Red = "red",
+  Green = "green",
+  Blue = "blue",
+}
+console.log(Color);  //=> { Red: 'red', Green: 'green', Blue: 'blue' }
+
+const myColor: Color = Color.Red;
+console.log(myColor);  //=> red
