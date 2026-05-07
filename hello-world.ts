71b82227b3fdd09b7aeefd2c8a1e2b09f9d75a2e
reonharuto <norabder567@gmail.com>
2026-05-08 04:54:19 +0900

    Complete TypeScript lint

--- hello-world.ts
+++ hello-world.ts
@@ -1,1 +1,10 @@
-export const helloWorld = "Hello World";
+type Choice = "A" | "B" | "C";
+
+export function func(choice: Choice) {
+  switch (choice) {
+    case "A":
+      break;
+    case "B":
+      break;
+  }
+}
