dabf0028effafdf711189f833b1908c848296718
reonharuto <norabder567@gmail.com>
2026-05-08 04:13:17 +0900

    Install @stylistic/eslint-plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,14 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
@@ -11,4 +17,11 @@ export default defineConfig([
     languageOptions: { globals: globals.browser }
   },
   tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "warn",
+      camelcase: ["warn", { properties: "never" }],
+      "@stylistic/semi": ["warn", "always"],
+    }
+  }
 ]);
